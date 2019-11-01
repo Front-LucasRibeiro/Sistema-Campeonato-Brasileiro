2b3a736af4263df69b743d8dc37f685b3f23a31f
Lucas Ribeiro <lksribeiro2014@gmail.com>
2019-11-01 01:30:08 -0300

    tabela de classificacao

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 var listaTimes = [
-{time:'Flamengo', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0},
-{time:'Palmeiras', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Santos', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'São Paulo', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Internacional', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Corinthians', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Grêmio', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Athletico-PR', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Bahia', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Goiás', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Vasco', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Atlético-MG', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Botafogo', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Fortaleza', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Ceará', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Fluminense', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Cruzeiro', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'CSA', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Chapecoense', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
-{time:'Avaí', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0 },
+{time:'Flamengo', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0},
+{time:'Palmeiras', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Santos', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'São Paulo', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Internacional', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Corinthians', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Grêmio', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Athletico-PR', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Bahia', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Goiás', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Vasco', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Atlético-MG', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Botafogo', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Fortaleza', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Ceará', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Fluminense', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Cruzeiro', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'CSA', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Chapecoense', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
+{time:'Avaí', pontos:0, vitorias: 0, derrotas:0, saldoGols: 0, empates:0 },
 ]
 
 
@@ -26,14 +26,14 @@ function gerarJogos(listaTimes) {
 
   var jogos = [];
   
-  for (var i=0; i<10; i++) {
+  for (var i=0; i<20; i++) {
 
     for (var j=0; j<20; j++) {
       if(listaTimes[i].time != listaTimes[j].time){
         jogos.push(listaTimes[i]);
         jogos.push(listaTimes[j]);
 
-        var tr = '<tr><td>'+listaTimes[i].time+'<td/><td class="gols"></td><td class="versus">x<td/><td class="gols"></td><td>'+listaTimes[j].time+'<td/><tr/>';
+        var tr = '<tr><td id="mandante">'+listaTimes[i].time+'<td/><td class="gols golsMandante"></td><td class="versus">x<td/><td class="gols golsAdversario"></td><td id="adversario">'+listaTimes[j].time+'<td/><tr/>';
         $('#listaJogos').append(tr);
       }
     }
@@ -42,31 +42,124 @@ function gerarJogos(listaTimes) {
   gerarGols(jogos)
 }
 
+function defineVencedor(listaTimes){
 
+  for(var l=0; l<380; l++){
+    
+    var golsMandante = document.querySelectorAll('#listaJogos tr .golsMandante')[l].innerText;
+    var golsAdversario = document.querySelectorAll('#listaJogos tr .golsAdversario')[l].innerText;
+    
 
+    // vitorias
+    if(parseInt(golsMandante) > parseInt(golsAdversario)){
+      var timeMandante = document.querySelectorAll('#mandante')[l].innerText;
 
-function gerarGols(jogos){
+      for(var t=0; t<20; t++){
+        
+        if(timeMandante == listaTimes[t].time){
 
-  for(var i=0; i<380; i++) {
-    var gols = Math.floor(Math.random() * 6);
+          listaTimes[t].vitorias += 1;
+          listaTimes[t].pontos += 3;
+          listaTimes[t].saldoGols += parseInt(golsMandante);
+        }
+      }
+    }else{
+      var timeAdversario = document.querySelectorAll('#adversario')[l].innerText;
 
-    var td = document.querySelectorAll('td.gols')[i];
-    td.innerText = jogos[i].saldoGols = gols;;
+      for(var t=0; t<20; t++){
+        
+        if(timeAdversario == listaTimes[t].time){
+
+          listaTimes[t].vitorias += 1;
+          listaTimes[t].pontos += 3;
+          listaTimes[t].saldoGols += parseInt(golsAdversario);
+        }
+      }
+    }
+
+    // derrotas
+    if(parseInt(golsMandante) < parseInt(golsAdversario)){
+      var timeMandante = document.querySelectorAll('#mandante')[l].innerText;
+
+      for(var t=0; t<20; t++){
+        
+        if(timeMandante == listaTimes[t].time){
+
+          listaTimes[t].derrotas += 1;
+        }
+      }
+    }else{
+      var timeAdversario = document.querySelectorAll('#adversario')[l].innerText;
+
+      for(var t=0; t<20; t++){
+        
+        if(timeAdversario == listaTimes[t].time){
+
+          listaTimes[t].derrotas += 1;
+        }
+      }
+    }
+
+    // empates
+    if(parseInt(golsMandante) === parseInt(golsAdversario)){
+      var timeMandante = document.querySelectorAll('#mandante')[l].innerText;
+
+      for(var t=0; t<20; t++){
+        
+        if(timeMandante == listaTimes[t].time){
+
+          listaTimes[t].empates += 1;
+          listaTimes[t].saldoGols += parseInt(golsMandante);
+        }
+      }
+    }else{
+      var timeAdversario = document.querySelectorAll('#adversario')[l].innerText;
+
+      for(var t=0; t<20; t++){
+        
+        if(timeAdversario == listaTimes[t].time){
+
+          listaTimes[t].empates += 1;
+          listaTimes[t].saldoGols += parseInt(golsAdversario);
+        }
+      }
+    }
   }
 }
 
 
+function gerarGols(jogos){
+
+  for(var i=0; i<760; i++) {
+    var gols = Math.floor(Math.random() * 6);
+
+    var td = document.querySelectorAll('td.gols')[i];
+    td.innerText = gols;
+  }
+}
 
 
+function criaTabelaClassificacao(listaTimes){
 
-
-
-
+  for (var n=0; n<20; n++) {
+    var tr = '<tr>\
+                <td>'+listaTimes[n].time+'<td/>\
+                <td>'+listaTimes[n].pontos+'<td/>\
+                <td>'+listaTimes[n].vitorias+'<td/>\
+                <td>'+listaTimes[n].derrotas+'<td/>\
+                <td>'+listaTimes[n].saldoGols+'<td/>\
+                <td>'+listaTimes[n].empates+'<td/>\
+              <tr/>';
+    $('#tabela-classificacao').append(tr);
+  }
+}
 
 
 $( document ).ready(function() {
 
   $('#montarCampeonato').click(function(){
     gerarJogos(listaTimes);
+    defineVencedor(listaTimes);
+    criaTabelaClassificacao(listaTimes);
   });
 });
